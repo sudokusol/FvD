1f31a28cdae557cfa5db06c34e8d04e4c2e371e5
lennyaaaa <112637527+lennyaaaa@users.noreply.github.com>
2024-11-20 23:56:56 +0100

    .

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const menuButton = document.querySelector('.menu button');
 const closeButton = document.querySelector('nav button');
 const navMenu = document.querySelector('nav');
+const searchBar = document.querySelector('.search-container');
 
 // Eventlisteners voor openen en sluiten van het menu
 menuButton.addEventListener('click', toonMenu);
@@ -8,8 +9,10 @@ closeButton.addEventListener('click', sluitMenu);
 
 function toonMenu() {
   navMenu.classList.add("toonMenu");
+  searchBar.classList.add("hide");
 }
 
 function sluitMenu() {
   navMenu.classList.remove("toonMenu");
+  searchBar.classList.remove("hide");
 }
